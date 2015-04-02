45aad1b1b48bf3db9576460868a6c23ac597c0d5
Ivan Valdes <iv@nvald.es>
2015-04-02 01:39:59 +0000

    Change folder structure

--- build.sh
+++ build.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-cp /usr/bin/etcdctl .
-cp /usr/bin/docker .
-cp /lib64/libdevmapper.so.1.02 .
-cp /lib64/libsqlite3.so.0 .
+cp /usr/bin/etcdctl files
+cp /usr/bin/docker files
+cp /lib64/libdevmapper.so.1.02 files
+cp /lib64/libsqlite3.so.0 files
 
 docker build -t ivan/docker-register .
--- files/docker-reg.sh
+++ files/docker-reg.sh
@@ -6,15 +6,13 @@ PORT=$3
 
 CTL="etcdctl -C http://${ETCD_PORT_10000_TCP_ADDR}:${ETCD_PORT_10000_TCP_PORT}"
 URI_NAME=$(echo $CONTAINER_NAME | awk '{ gsub(/_/, "-"); print }')
-PUBLISHED_PORT=""
-[[ -z $PORT ]] && PUBLISHED_PORT=$(/usr/bin/docker port $CONTAINER_NAME $PORT | awk -F':' '{print $2}')
 
 KEY="/services/${SERVICE}/${URI_NAME}"
 trap "$CTL rm $KEY; exit" SIGHUP SIGINT SIGTERM
 
 while [ 1 ]; do
   IP=$(/usr/bin/docker inspect $CONTAINER_NAME | grep IPAddress | awk '{ gsub(/[^0-9\.]/, ""); print }')
-  VALUE="{\"IP\":\"${IP}\",\"port\":\"${PUBLISHED_PORT}\"}"
+  VALUE="{\"IP\":\"${IP}\",\"port\":\"${PORT}\"}"
 
   $CTL --debug set "$KEY" "$VALUE" --ttl 5
   sleep 1
